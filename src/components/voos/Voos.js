3ac20faae332fad2bce176fd038f93e5e673c436
Radio Operator <Buzios.Radiooper@vessels.dof.no>
2021-03-21 03:31:15 -0300

    ajuste nos voos layout e display

--- src/components/voos/Voos.js
+++ src/components/voos/Voos.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
+import moment from "moment";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
 import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
-import CardContent from "@material-ui/core/CardContent";
-import Box from "@material-ui/core/Box";
-import Chip from "@material-ui/core/Chip";
-import Card from "@material-ui/core/Card";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 import red from "@material-ui/core/colors/red";
 import grey from "@material-ui/core/colors/grey";
 import vesselContext from "../../context/details/vesselContext";
@@ -39,9 +40,6 @@ const useStyles = makeStyles({
     textAlign: "center",
     backgroundColor: grey[900],
     color: "white"
-  },
-  distancia: {
-    marginBottom: 20
   }
 });
 
@@ -54,148 +52,59 @@ export default function Voos() {
   }, []);
 
   const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
 
   const VesselContext = useContext(vesselContext);
 
-  const {
-    getBuziosWeather,
-    weather,
-    diaHoje,
-    getVoosBuzios,
-    voosBuzios
-  } = VesselContext;
+  const { getBuziosWeather, getVoosBuzios, voosBuzios } = VesselContext;
 
-  const prevHoje = weather.filter(i => i.data === diaHoje);
+  const handleChanges = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
-  const ondaHoje = prevHoje.map(i => i.onda);
-  const ondaString = ondaHoje.toString();
-  const ondaReduz = ondaString.substr(0, 3);
-  const ondaFloat = parseFloat(ondaReduz);
+  const voosDeHoje = voosBuzios.filter(
+    i => i.data === moment().format("DD/MM/YYYY")
+  );
 
   return (
     <React.Fragment>
       <Grid
-        spacing={2}
         container
+        direction="row"
         justify="center"
         alignItems="center"
-        style={{ backgroundColor: "#37474f", marginBottom: 20 }}
+        style={{ marginBottom: 20 }}
       >
-        {voosBuzios.map(i => (
-          <Grid
-            item
-            xs={12}
-            sm={6}
-            md={4}
-            key={i.id}
-            style={{ marginTop: 30, marginBottom: 30 }}
-          >
-            <Card>
-              <CardContent>
-                <div className={classes.empresa} variant="square">
-                  <img
-                    src={"https://www.tusas.com/uploads/yapisal-10.png"}
-                    alt="Companhia Aerea"
-                    style={{
-                      backgroundColor: "#fff",
-                      width: 80
-                    }}
-                  />
-                  <img
-                    src={
-                      i.tempoAero
-                        ? i.tempoAero
-                        : "https://www.windy.com/img/icons5/png_25px/1_night_7.png"
-                    }
-                    alt="Clima em Jacarepagua"
-                    style={{ float: "right" }}
-                  />
-                </div>
-                <Chip
-                  label={i.modelo}
-                  color="primary"
-                  variant="outlined"
-                  size="small"
-                />
-
-                <Box align="center">
-                  <Typography variant="h5" component="h2">
-                    {i.horario}
-                  </Typography>
-                  <Typography color="textSecondary">{i.prefixo}</Typography>
-                  <Typography variant="subtitle2">{i.numero}</Typography>
-                </Box>
-
-                <Grid container justify="center" style={{ marginTop: 10 }}>
-                  <Grid item>
-                    <Chip label={i.procedencia} color="default" />
-                  </Grid>
-                </Grid>
-                <Grid container justify="center" style={{ marginTop: 10 }}>
-                  <Grid item>
-                    <Chip label={i.obs} color="primary" />
-                  </Grid>
-                </Grid>
-                <Grid container justify="center">
-                  <Grid item style={{ marginTop: 10 }}>
-                    <Box align="right" className={classes.corpoMensagem}>
-                      <Typography
-                        variant="subtitle2"
-                        gutterBottom
-                        style={{ textAlign: "center", marginBottom: 10 }}
-                      >
-                        Briefing Buzios Previsto {i.horario}
-                        {i.saida_aero.length > 0 && ondaFloat < 2 && (
-                          <Typography
-                            variant="body2"
-                            style={{ textAlign: "center" }}
-                          >
-                            Confirmar horario de Decolagem com a Sala de Radio.
-                            Em alguns casos o Prefixo da Aeronave pode mudar e o
-                            horario nao estar certo.
-                          </Typography>
-                        )}
-                        {ondaFloat < 2 && i.saida_aero.length === 0 && (
-                          <Typography
-                            variant="body2"
-                            style={{ textAlign: "center" }}
-                          >
-                            Assim que a Aeronave Decolar será informado Horário
-                            e ETA.{" "}
-                          </Typography>
-                        )}
-                        {ondaFloat > 2 && (
-                          <Typography
-                            variant="body2"
-                            style={{ textAlign: "center" }}
-                          >
-                            As condições de mar estão oscilando ou fora do
-                            limite para atendimento de Aeronave. O horário do
-                            voo pode mudar ou ser transferido para amanhã.
-                          </Typography>
-                        )}
-                      </Typography>
-                    </Box>
-                    {i.saida_aero.length > 0 && ondaFloat < 2 && (
-                      <Grid item>
-                        <Paper elevation={3} className={classes.decolagem}>
-                          <Typography variant="h6">
-                            Decolado as {i.saida_aero}
-                          </Typography>
-                        </Paper>
-                      </Grid>
-                    )}
-                    {i.eta.length > 0 && ondaFloat < 2 && (
-                      <Grid item>
-                        <Paper elevation={3} className={classes.eta}>
-                          <Typography variant="h6">ETA as {i.eta}</Typography>
-                        </Paper>
-                      </Grid>
-                    )}
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
+        {voosDeHoje.map(i => (
+          <Grid item xs={12} key={i.id}>
+            <Accordion
+              expanded={expanded === i.id}
+              onChange={handleChanges(i.id)}
+              key={i.id}
+            >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1bh-content"
+                id="panel1bh-header"
+              >
+                <Typography className={classes.heading}>{i.data}</Typography>
+                <Typography className={classes.secondaryHeading}>
+                  {" - "}
+                  {i.horario}
+                  <strong>
+                    {" - "}
+                    {i.prefixo}
+                  </strong>
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  {i.procedencia}
+                  {" / "}
+                  {i.modelo} {" / "} {i.empresa_tt}
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
           </Grid>
         ))}
       </Grid>
